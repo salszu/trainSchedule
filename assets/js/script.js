2ce69ba60feb98776abaed207b49a41a1d3bbe29
Salwador Szurlej <sal.szu@gmail.com>
2016-06-19 20:43:29 -0400

    added font awesome icons and reset database after testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,55 @@
+//firebase
+var trainInput = new Firebase('https://trainsnthings.firebaseio.com/');
+//input variables
+var name = '';
+var frequency = '';
+var time = '';
+var destination = '';
+//add train button
+$('#submitTrain').on('click', function() {
+    //user input
+    name = $('#trainName').val().trim();
+    destination = $('#trainDestination').val().trim();
+    time = moment($('#trainTime').val().trim(), "HH:mm").subtract(10, "years").format("X");
+    frequency = $('#trainFrequency').val().trim();
+    //push to firebase
+    trainInput.push({
+        name: name,
+        destination: destination,
+        time: time,
+        frequency: frequency,
 
-		//firebase
-    var trainInput = new Firebase('https://trainsnthings.firebaseio.com/');
-    	//input variables
-    var name = '';
-    var frequency = '';
-    var time = '';
-    var destination = '';
-    	//add train button
-    $('#submitTrain').on('click', function() {
-    		//user input
-        name = $('#trainName').val().trim();
-        destination = $('#trainDestination').val().trim();
-        time = moment($('#trainTime').val().trim(), "HH:mm").subtract(10, "years").format("X");
-        frequency = $('#trainFrequency').val().trim();
-        	//push to firebase
-        trainInput.push({
-            name: name,
-            destination: destination,
-            time: time,
-            frequency: frequency,
-
-        });
-        	//empty input boxes after submit
-        $('#trainName').val('');
-        $('#trainDestination').val('');
-        $('#trainTime').val('');
-        $('#trainFrequency').val('');
-
-        return false;
     });
+    //empty input boxes after submit
+    $('#trainName').val('');
+    $('#trainDestination').val('');
+    $('#trainTime').val('');
+    $('#trainFrequency').val('');
 
-
-trainInput.on("child_added", function(childSnapshot, prevChildKey){
-		//new var for input
-	var inputName = childSnapshot.val().name;
-	var inputDestination = childSnapshot.val().destination;
-	var inputFrequency = childSnapshot.val().frequency;
-	var inputFirstTrain = childSnapshot.val().time;
-
-	var differenceTimes = moment().diff(moment.unix(inputFirstTrain), "minutes");
-	var trainRemainder = moment().diff(moment.unix(inputFirstTrain), "minutes") % inputFrequency;
-	var trainMinutes = inputFrequency - trainRemainder;
-
-	var trainArrival = moment().add(trainMinutes, "m").format("hh:mm A"); 
-	console.log(trainMinutes);
-	console.log(trainArrival);
-
-	console.log(moment().format("hh:mm A"));
-	console.log(trainArrival);
-	console.log(moment().format("X"));
-
-		//append trainInfo table
-	$("#trainInfo > tbody").append("<tr><td>" + inputName + "</td><td>" + inputDestination + "</td><td>" + inputFrequency + "</td><td>" + trainArrival + "</td><td>" + trainMinutes + "</td></tr>");
-
+    return false;
 });
 
-  
+
+trainInput.on("child_added", function(childSnapshot, prevChildKey) {
+    //new var for input
+    var inputName = childSnapshot.val().name;
+    var inputDestination = childSnapshot.val().destination;
+    var inputFrequency = childSnapshot.val().frequency;
+    var inputFirstTrain = childSnapshot.val().time;
+
+    var differenceTimes = moment().diff(moment.unix(inputFirstTrain), "minutes");
+    var trainRemainder = moment().diff(moment.unix(inputFirstTrain), "minutes") % inputFrequency;
+    var trainMinutes = inputFrequency - trainRemainder;
+
+    var trainArrival = moment().add(trainMinutes, "m").format("hh:mm A");
+    console.log(trainMinutes);
+    console.log(trainArrival);
+
+    console.log(moment().format("hh:mm A"));
+    console.log(trainArrival);
+    console.log(moment().format("X"));
+
+    //append trainInfo table
+    $("#trainInfo > tbody").append("<tr><td>" + inputName + "</td><td>" + inputDestination + "</td><td>" + inputFrequency + "</td><td>" + trainArrival + "</td><td>" + trainMinutes + "</td></tr>");
+
+});
